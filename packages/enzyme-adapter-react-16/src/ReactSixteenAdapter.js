f7ea470f0527cce346623cc6cc81beb308f77467
Winton Welsh <mail@wintoni.us>
2018-05-17 16:09:47 -0400

    Point to @invrs/enzyme in react-16

--- packages/enzyme-adapter-react-16/src/ReactSixteenAdapter.js
+++ packages/enzyme-adapter-react-16/src/ReactSixteenAdapter.js
@@ -7,7 +7,7 @@ import ReactDOMServer from 'react-dom/server';
 import ShallowRenderer from 'react-test-renderer/shallow';
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import TestUtils from 'react-dom/test-utils';
-import { EnzymeAdapter } from 'enzyme';
+import { EnzymeAdapter } from '@invrs/enzyme';
 import {
   elementToTree,
   nodeTypeFromType,
